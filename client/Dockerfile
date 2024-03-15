516c184b6bfb407ad81402a5f5aa5a0aa7ef8ae0
Bruno Salmon <bruno@payslip.com>
2024-03-15 15:35:44 +0100

    added client

---
+++ client/Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="bruno"
+
+ENTRYPOINT ["top", "-b"]
---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import React from "react";
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Fib from "./Fib";
+import OtherPage from "./OtherPage";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className='App-title'>Welcome to React</h1>
+          <Link to='/'>Home</Link>
+          <Link to='/otherPage'></Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route exact path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
